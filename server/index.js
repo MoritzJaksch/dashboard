3baa0391a442c7e570e319cc8430db1a1d6132af
Moritz Jaksch <moritz.jaksch@me.com>
2019-03-22 16:22:14 +0100

    deploying test

--- server/index.js
+++ server/index.js
@@ -81,6 +81,13 @@ app.get("/api/twitter/user", (req, res) => {
     );
 });
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(__dirname + "/public"));
+    app.get(/.*/, (req, res) => {
+        res.sendFle(__dirname + "/public/index.html");
+    });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
